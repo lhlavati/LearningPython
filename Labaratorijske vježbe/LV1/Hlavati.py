ce899df3640972aee3f8434b7b5f2046652621cc
lhlavati <lukahlavati@gmail.com>
2019-10-09 16:13:11 +0200

    LV1 Solved tasks

---
+++ Labaratorijske vježbe/LV1/Hlavati.py
@@ -0,0 +1,67 @@
+#TASK 1
+def count_evens(lst):
+	count = 0
+	for number in lst:
+		if number % 2 == 0:
+			count += 1
+	return count
+		
+print (count_evens([2,3,4,5,6,7,8,9]))
+
+#TASK 2
+def centered_average(lst):
+        sortedList = sorted(lst)
+        sortedList.pop(0)
+        sortedList.pop(len(sortedList)-1)
+        print (sortedList)
+        if len(sortedList) % 2 != 0:
+                return sortedList[len(sortedList) // 2]
+        else:
+                middle = (len(sortedList) // 2) - 1
+                median = (sortedList[middle] + sortedList[middle + 1]) // 2
+                return median
+  
+print (centered_average([1,2,3,4,5,6]))
+
+#TASK 3
+def next(lst):
+        for index in range(len(lst)-1):
+                if lst[index] == 2 and lst[index+1] == 2:
+                        return True
+        else:
+                return False
+                
+print (next([1,2,3,2,3,4,5,6,7,2,3,2]))
+
+
+####### practicepython.org #######
+
+#TASK 3
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for number in a:
+        if number < 5:
+                print (number)
+
+#TASK 11
+def is_prime(x):
+    if x < 2:
+        return False
+    else:
+        for n in range(2, x-1):
+            if x % n == 0:
+                return False
+        return True
+
+print (is_prime(9))
+
+#TASK 6
+def palindrome():
+        string = input("Enter a word: ")
+        stringBackwards = string [::-1]
+        string.lower()
+        if string == stringBackwards:
+                return print ("String is a palindrome")
+        else:
+                return print ("String is not a palindrome")
+
+palindrome()
